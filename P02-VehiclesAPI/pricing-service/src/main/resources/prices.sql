57267d439732e33714c1e24a20e3d035d49c5735
Rafael Ricardo <rafael_rikardo@yahoo.com.br>
2021-06-05 00:10:10 +0100

    Eureka server created and working. Pricing service is a microservice now but still not using data rest.

---
+++ P02-VehiclesAPI/pricing-service/src/main/resources/prices.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO price (vehicleId, currency, price) VALUES (1, 'USD', 100);
+INSERT INTO price (vehicleId, currency, price)) VALUES (2,'USD', 200);
+INSERT INTO price (vehicleId, currency, price) VALUES (3, 'USD', 300);
+INSERT INTO price (vehicleId, currency, price) VALUES (4, 'USD', 400);
+INSERT INTO price (vehicleId, currency, price) VALUES (5, 'USD', 500);
