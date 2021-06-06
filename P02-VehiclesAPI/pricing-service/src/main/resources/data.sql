58de3aa469664b6f9f81e8bd8cf804c1c89da5dd
Rafael Ricardo <rafael_rikardo@yahoo.com.br>
2021-06-06 11:09:07 +0100

    Pricing service fixed. The file prices.sql was changed to data.sql then spring-boot could insert the prices in the database in memory.

---
+++ P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO Price (currency, price, vehicle_id) VALUES ('EUR', 1456.15, 1);
+INSERT INTO Price (currency, price, vehicle_id) VALUES ('EUR', 2340.00, 2);
+INSERT INTO Price (currency, price, vehicle_id) VALUES ('EUR', 960.76, 3);
+INSERT INTO Price (currency, price, vehicle_id) VALUES ('EUR', 4499.99, 4);
+INSERT INTO Price (currency, price, vehicle_id) VALUES ('EUR', 3070.50, 5);
--- P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/client/prices/PriceClient.java
+++ P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/client/prices/PriceClient.java
@@ -35,8 +35,9 @@ public class PriceClient {
             Price price = client
                     .get()
                     .uri(uriBuilder -> uriBuilder
-                            .path("services/price/")
-                            .queryParam("vehicleId", vehicleId)
+                            .path("prices/"+vehicleId)
+                            //.path("services/price/")
+                            //.queryParam("vehicleId", vehicleId)
                             .build()
                     )
                     .retrieve().bodyToMono(Price.class).block();
